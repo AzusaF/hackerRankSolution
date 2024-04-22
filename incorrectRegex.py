48635c30b0512fe334d939ac601e56ec439b0073
Azusa Fukuda <95828247+AzusaF@users.noreply.github.com>
2024-04-21 22:40:59 -0400

    Solve Incorrect Regex

---
+++ incorrectRegex.py
@@ -0,0 +1,14 @@
+# Incorrect Regex
+
+import re
+
+for _ in range(int(input())):
+  try:
+    regex = input()
+    if '*+' in regex or '++' in regex or '+?' in regex:
+      print("False")
+    else:
+      re.compile(regex)
+      print("True")
+  except re.error:
+    print("False")
